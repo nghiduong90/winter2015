6663577838caf4eddf4cce84f43da2506c01c9eb
Nghi Duong <nghiduong90@gmail.com>
2015-02-04 16:09:13 -0800

    first commit

---
+++ js/introHCI.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$("#testjs").click(function(e) {
+		$('.jumbotron h1').text("Javascript has taken control");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
+		$(".jumbotron p").toggleClass("active");
+		$("#submitBtn").click(updateProject); 
+	});
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+    // debug message
+    console.log("mouse clicked");
+
+	// prevent the page from reloading
+	e.preventDefault();
+
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	jumbotronHeader.text(projectTitle);
+
+	var containProject = $(this).closest(".project");
+	var Description = $(containProject).find(".project-description");
+
+	if (Description.length == 0) {
+		$(containProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	}
+	else
+		Description.fadeOut();
+		//Description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	
+	//containProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+	// In an event handler, $(this) refers to
+	// the onject the triggered the event
+	//$(this).css("background-color", "#7fff00");
+}
+
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
+}
